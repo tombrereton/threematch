e0e04de218bae20c15e4aa190a25571d78505f68
elliottdavies <elliottdavies94@gmail.com>
2017-06-04 12:56:11 +0100

    Changed gemgrid

--- gemgrid.py
+++ gemgrid.py
@@ -17,8 +17,20 @@ class Gem(pygame.sprite.Sprite):
         self.gem_name = "stones/Stone_0{}_05.png".format(self.type)
         self.image, self.rect = util.load_image(self.gem_name, size)
         self.dizzy = 0
+        self.target_y = 0
+        self.target_x = 0
+
+    def init_rect(self, y: int, x: int):
+        self.rect.left = x
+        self.rect.top = y
+        self.set_target(y, x)
+
+    def set_target(self, y: int, x: int):
+        self.target_y = y
+        self.target_x = x
 
     def test_gem(self, size: int, type: int):
+        self.type = type
         test_gem = "stones/Stone_0{}_05.png".format(type)
         self.image, self.rect = util.load_image(test_gem, size)
 
@@ -97,8 +109,7 @@ class GemGrid(g.Grid):
         gem = Gem(self.gem_size)
         x = self.margin + self.centering_offset + x_coord * self.cell_size
         y = self.margin + self.centering_offset + y_coord * self.cell_size
-        gem.rect.left = x
-        gem.rect.top = y
+        gem.init_rect(y, x)
         self.grid[y_coord][x_coord] = gem
 
     def removegem(self, y_coord: int, x_coord: int):
