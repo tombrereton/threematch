c8746ee60ba29e8f2235dc2d2a6ed6d698f57c1d
tombrereton <tbrereton89@gmail.com>
2017-06-20 18:44:40 +0100

    Tests 1.7 not passing.

--- game_test.py
+++ game_test.py
@@ -17,7 +17,7 @@ medals1 = 0
 moves_left1 = 30
 
 
-def test_1_use_bonus_type_1():
+def test_1_1_use_bonus_type_1():
     """
     Should remove all gems in row and create no bonuses
     :return:
@@ -49,7 +49,7 @@ def test_1_use_bonus_type_1():
     assert expected_bonuses == actual_bonuses
 
 
-def test_2_use_bonus_type_2():
+def test_1_2_use_bonus_type_2():
     """
     TODO
     :return:
@@ -57,7 +57,7 @@ def test_2_use_bonus_type_2():
     print('\n\nTest use bonus type 2:\n')
 
 
-def test_3_use_bonus_type_3():
+def test_1_3_use_bonus_type_3():
     """
     TODO
     :return:
@@ -65,14 +65,14 @@ def test_3_use_bonus_type_3():
     print('\n\nTest use bonus type 3:\n')
 
 
-def test_4_use_4_match_intersect():
+def test_1_4_use_4_match_intersect():
     """
     TODO
     :return:
     """
     print('\n\nTest 4 match intersect:\n')
 
-    b2 = Board(4, 4, ice_rows1, medals1, moves_left1, test='horizontal', event_manager=event_manager)
+    b2 = Board(4, 4, ice_rows1, medals1, moves_left1, gem_types=4, test='horizontal', event_manager=event_manager)
 
     # Set up grid for testing
     b2.gem_grid.grid[0][2] = (2, 0, 0)
@@ -100,7 +100,95 @@ def test_4_use_4_match_intersect():
     assert expected_bonuses == actual_bonuses
 
 
-def test_5_ice_removed():
+def test_1_5_use_bonus_3_intersect():
+    """
+    TODO
+    :return:
+    """
+    print('\n\nTest 1.5 match intersect:\n')
+
+    b2 = Board(3, 4, ice_rows1, medals1, moves_left1, gem_types=3, test='horizontal', event_manager=event_manager)
+
+    # Set up grid for testing
+    b2.gem_grid.grid[0][2] = (2, 0, 0)
+    b2.gem_grid.grid[1][2] = (2, 0, 0)
+    b2.gem_grid.grid[2][2] = (0, 0, 0)
+    b2.gem_grid.grid[2][3] = (2, 3, 0)
+    print(b2)
+
+    # swap to allow matches to be found
+    swap_locations = [(2, 2), (2, 3)]
+    b2.set_swap_locations(swap_locations)
+
+    # find matches by calling get update twice
+    bag = b2.get_update()
+    bag = b2.get_update()
+    print(bag)
+
+    expected_removals = [(0, 2, 2, 0, 0), (1, 1, 1, 0, 0), (1, 2, 2, 0, 0), (1, 3, 1, 0, 0), (2, 0, 2, 0, 0),
+                         (2, 1, 2, 0, 0), (2, 3, 0, 0, 0)]
+    expected_bonuses = [(2, 2, 2, 3, 0)]
+
+    actual_removals = bag.removals
+    actual_bonuses = bag.bonuses
+
+    assert expected_removals == actual_removals
+    assert expected_bonuses == actual_bonuses
+
+
+def test_1_6_use_bonus_2_get_bonus_1():
+    """
+    Testing that a bonus type 1 is still
+    created when using a bonus type 2.
+
+    Bonus type 1 occurs from 4 in a row.
+    Bonus type 2 destroys all gems of that type.
+    :return:
+    """
+    print('\n\nTest 1.6 using a start bonus and getting a match 4:\n')
+
+    b2 = Board(1, 8, ice_rows1, medals1, moves_left1, gem_types=3, test='horizontal', event_manager=event_manager)
+
+    # Set up grid for testing
+    b2.gem_grid.grid[0][4] = (1, 0, 0)
+    b2.gem_grid.grid[0][5] = (0, 2, 0)
+    print(b2)
+
+    expected_removals = [(0, 1, 0, 0, 0), (0, 2, 0, 0, 0), (0, 3, 0, 0, 0), (0, 5, 0, 2, 0),
+                         (0, 6, 0, 0, 0), (0, 7, 0, 0, 0)]
+    expected_bonuses = [(0, 0, 0, 1, 0)]
+
+    actual_removals, actual_bonuses = b2.find_matches()
+
+    assert expected_removals == actual_removals
+    assert expected_bonuses == actual_bonuses
+
+
+def test_1_7_use_bonuses_when_removed_by_bonus():
+    """
+    TODO
+    :return:
+    """
+    print('\n\nTest 1.7 using a bonus when it is removed by a bonus:\n')
+
+    b2 = Board(1, 5, ice_rows1, medals1, moves_left1, gem_types=3, test='horizontal', event_manager=event_manager)
+
+    # Set up grid for testing
+    b2.gem_grid.grid[0][0] = (0, 3, 0)
+    b2.gem_grid.grid[0][1] = (1, 0, 0)
+    b2.gem_grid.grid[0][2] = (0, 2, 0)
+    print(b2)
+
+    expected_removals = [(0, 0, 0, 3, 0), (0, 1, 1, 0, 0), (0, 2, 0, 2, 0), (0, 3, 0, 0, 0), (0, 4, 0, 0, 0)]
+    expected_bonuses = []
+
+    actual_removals, actual_bonuses = b2.find_matches()
+
+    assert expected_removals == actual_removals
+    assert expected_bonuses == actual_bonuses
+
+
+def test_2_1_ice_removed():
     print("\nBoard 1:\n")
     print(b)
 
@@ -135,7 +223,7 @@ def test_5_ice_removed():
     assert current_ice == ice_grid
 
 
-def test_6_medals_removed():
+def test_2_2_medals_removed():
     medal_grid = [[-1] * columns for _ in range(rows)]
     current_medals = b.medal_grid.grid
     assert current_medals == medal_grid
