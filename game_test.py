a0faea5dc697969b9472d7234408afe09aaec525
tombrereton <tbrereton89@gmail.com>
2017-06-20 17:00:47 +0100

    Created test_use_bonus_type_1

--- game_test.py
+++ game_test.py
@@ -10,6 +10,45 @@ moves_left = 30
 event_manager = EventManager()
 b = Board(rows, columns, ice_rows, medals, moves_left, test='horizontal', event_manager=event_manager)
 
+rows1 = 1
+columns1 = 5
+ice_rows1 = 0
+medals1 = 0
+moves_left1 = 30
+
+event_manager = EventManager()
+b1 = Board(rows1, columns1, ice_rows1, medals1, moves_left1, test='horizontal', event_manager=event_manager)
+
+
+def test_use_bonus_type_1():
+    """
+    Should remove all gems in row and create no bonuses
+    :return:
+    """
+    print('\n\nTest use bonus type 1:\n')
+
+    # Set up grid for testing
+    b1.gem_grid.grid[0][3] = (0, 1, 0)
+    b1.gem_grid.grid[0][4] = (1, 0, 0)
+
+    # swap to allow matches to be found
+    swap_locations = [(0, 0), (0, 1)]
+    b1.set_swap_locations(swap_locations)
+
+    # find matches by calling get update twice
+    bag1 = b1.get_update()
+    bag = b1.get_update()
+
+    expected_removals = [(0, 0, 0, 0, 0), (0, 1, 0, 0, 0), (0, 2, 0, 0, 0), (0, 3, 0, 1, 0), (0, 4, 1, 0, 0)]
+    expected_bonuses = []
+
+    actual_removals = bag.removals
+    actual_bonuses = bag.bonuses
+
+    assert expected_removals == actual_removals
+    assert expected_bonuses == actual_bonuses
+
+
 
 def test_ice_removed():
     print("\nBoard 1:\n")
