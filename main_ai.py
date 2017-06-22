abc7458afe6b63a0ecc118fab1c06e01f54c04d3
tombrereton <tbrereton89@gmail.com>
2017-06-22 21:05:48 +0100

    Cleaned up v1 AI controller so that all object needed are passed thru' the constructor.

--- main_ai.py
+++ main_ai.py
@@ -3,6 +3,7 @@ from game import Board
 from global_variables import *
 from gui import GUI
 from gui_variables import *
+from move_finder import pick_move
 
 
 # ------------------------------------------------------------------------------
@@ -15,7 +16,7 @@ def main():
     evManager = EventManager(gui_vars)
 
     game_board = Board(PUZZLE_ROWS, PUZZLE_COLUMNS, ICE_ROWS, LEVEL_1_TOTAL_MEDALS, MOVES_LEFT, event_manager=evManager)
-    ai_cont = NaiveAIControllerV1(evManager, game_board)
+    ai_cont = NaiveAIControllerV1(evManager, game_board, pick_move)
     mouse_cont = MouseController(evManager)
     spinner = CPUSpinnerController(evManager)
     view = GUI(gui_vars, *game_board.state(), event_manager=evManager)
