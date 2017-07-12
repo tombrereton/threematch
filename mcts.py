04c0230a9a0c883052193ad6f04cfbf86dcaa3fc
elliottdavies <elliottdavies94@gmail.com>
2017-07-12 21:56:06 +0100

    Added pick_move method to mcts.py

--- mcts.py
+++ mcts.py
@@ -10,28 +10,39 @@ class MonteCarlo:
     def update(self, state):
         self.states.append(state)
 
+    def pick_move(self, game_limit, move_limit):
+        for _ in range(game_limit):
+            self.play(move_limit)
+
+        moves = self.board.moves(self.states)
+        child_states = [self.board.transition(self.states, move) for moves]
+        stats = [self.statistics.get((state, move)) for state, move in zip(child_states, moves)]
+        
+        move, *_ = max(zip(moves, child_states, stats), key=(lambda el: el[2][1] / el[2][0]))
+        
+        return move
+
     def play(self, move_limit):
         visited = set()
         states = [*self.states]
-        
+
         for _ in range(move_limit):
             moves = self.board.moves(states)
-            
+
             if not moves:
                 break
-            
+
             move = random.choice(moves)
             state = self.board.transition(states, move)
             states.add(state)
             visisted.add((state, move))
-        
+
         winner = self.board.winner(states)
-        
+
         for state, move in visisted:
             if self.statistics.get(state, False):
-                self.statistics[state].plays += 1
+                self.statistics[state][0] += 1
                 if winner:
-                    self.statistics[state].wins += 1
+                    self.statistics[state][1] += 1
             else:
-                self.statistics[state].plays = 1
-                self.statistics[state].wins = 1 if winner else 0
+                self.statistics[state] = [1, 1 if winner else 0]
