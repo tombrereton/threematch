32e666f34bf5f1634e801eecb1779b2b80a3742a
elliottdavies <elliottdavies94@gmail.com>
2017-07-12 21:15:47 +0100

    Added some statistic collection to play method in mcst.py

--- mcst.py
+++ mcst.py
@@ -5,13 +5,14 @@ class MonteCarlo:
     def __init__(self, board):
         self.board = board
         self.states = []
+        self.statistics = {}
 
     def update(self, state):
         self.states.append(state)
 
     def play(self, move_limit):
-        states = self.states[:]
-        state = states[-1]
+        visited = set()
+        states = [*self.states]
         
         for _ in range(move_limit):
             moves = self.board.moves(states)
@@ -21,3 +22,16 @@ class MonteCarlo:
             
             move = random.choice(moves)
             state = self.board.transition(states, move)
+            states.add(state)
+            visisted.add((state, move))
+        
+        winner = self.board.winner(states)
+        
+        for state, move in visisted:
+            if self.statistics.get(state, False):
+                self.statistics[state].plays += 1
+                if winner:
+                    self.statistics[state].wins += 1
+            else:
+                self.statistics[state].plays = 1
+                self.statistics[state].wins = 1 if winner else 0
