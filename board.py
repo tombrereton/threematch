e89244bf331a252823b345c0562660fdeed13ab3
elliottdavies <elliottdavies94@gmail.com>
2017-07-12 18:52:05 +0100

    Added comments to board.py

--- board.py
+++ board.py
@@ -1,10 +1,19 @@
 class Board:
 
     def __init__(self, size):
+        """
+        Set up the board
+        """
         pass
     
     def moves(self, states):
+        """
+        Returns a list of possible moves
+        """
         pass
     
     def transition(self, state, move):
+        """
+        Return the next state given the current state and move
+        """
         pass
