b419115659eec99cd56817c9300ceb60342ca4ad
elliottdavies <elliottdavies94@gmail.com>
2017-07-28 16:52:41 +0100

    Reformatted blockbusters example

--- ai/mcts_trial/blockbusters.py
+++ ai/mcts_trial/blockbusters.py
@@ -1,11 +1,10 @@
 import random
-import sys
 from itertools import product
 
 from ai.mcts_trial.board import Board
 from ai.mcts_trial.mcts import MonteCarlo
 
-size = int(sys.argv[1])
+size = 5
 
 
 def print_board(state):
@@ -16,7 +15,6 @@ def print_board(state):
 
 
 class Blockbusters(Board):
-
     def __init__(self):
         pass
 
@@ -25,11 +23,11 @@ class Blockbusters(Board):
         return ((0,) * size,) * size, 1
 
     def players(self):
-        return (1, 2)
+        return 1, 2
 
     def player(self, states):
         return states[-1][1]
-    
+
     def winner(self, states):
         global size
         grid = states[-1][0]
@@ -39,12 +37,13 @@ class Blockbusters(Board):
         while to_check:
             coord = to_check.pop()
             checked.append(coord)
-            new = [(coord[0] - 1, coord[1]), (coord[0] + 1, coord[1]), (coord[0], coord[1] - 1), (coord[0], coord[1] + 1)]
+            new = [(coord[0] - 1, coord[1]), (coord[0] + 1, coord[1]), (coord[0], coord[1] - 1),
+                   (coord[0], coord[1] + 1)]
             if size in [i[1] for i in new]:
                 return 1
             new = [i for i in new if i not in checked]
             new = [i for i in new if 0 <= i[0] < size and 0 <= i[1] < size]
-            new = [i for i in new if grid[i[0]][i[1]] == 1]           
+            new = [i for i in new if grid[i[0]][i[1]] == 1]
             to_check.extend(new)
         # check if player two has a vertical
         checked = []
@@ -52,15 +51,15 @@ class Blockbusters(Board):
         while to_check:
             coord = to_check.pop()
             checked.append(coord)
-            new = [(coord[0] - 1, coord[1]), 
-            (coord[0] + 1, coord[1]), 
-            (coord[0], coord[1] - 1), 
-            (coord[0], coord[1] + 1)]
+            new = [(coord[0] - 1, coord[1]),
+                   (coord[0] + 1, coord[1]),
+                   (coord[0], coord[1] - 1),
+                   (coord[0], coord[1] + 1)]
             if size in [i[0] for i in new]:
                 return 2
             new = [i for i in new if i not in checked]
             new = [i for i in new if 0 <= i[0] < size and 0 <= i[1] < size]
-            new = [i for i in new if grid[i[0]][i[1]] == 2]  
+            new = [i for i in new if grid[i[0]][i[1]] == 2]
             to_check.extend(new)
         return 0
 
@@ -82,7 +81,8 @@ class Blockbusters(Board):
         new_grid = [[*grid[i]] for i in range(size)]
         new_grid[move[0]][move[1]] = move[2]
         new_grid = tuple(tuple(row) for row in new_grid)
-        return (new_grid, 3 - player)
+        return new_grid, 3 - player
+
 
 def recur_len(a):
     if type(a) == tuple:
@@ -90,31 +90,30 @@ def recur_len(a):
     else:
         return 1
 
-bb = Blockbusters()
-states = [bb.start()]
-print_board(states[-1])
-i = 0
-mc = MonteCarlo(bb, int(sys.argv[2]), int(sys.argv[3]))
-mc.update(states[-1])
-while True:
-    if i:
-        moves = tuple(bb.moves(states))
-        if not moves:
-            break
-        move = random.choice(moves)
-    else:
-        move = mc.pick_move()
-        if not move:
-            break
-    new_state = bb.transition(states, move)
-    states.append(new_state)
-    mc.update(new_state)
-    print_board(new_state)
-    i = 1 - i
-print(bb.winner(mc.states))
-# s = mc.statistics
-# c = 0
-# for key, value in s.items():
-#     c += recur_len(key)
-#     c += len(value)
-# print(4 * c, 'bytes')
+
+def main():
+    bb = Blockbusters()
+    states = [bb.start()]
+    print_board(states[-1])
+    i = 0
+    mc = MonteCarlo(bb, 100, 100, 2 ** 0.5)
+    mc.update(states[-1])
+    while True:
+        if i:
+            moves = tuple(bb.moves(states))
+            if not moves:
+                break
+            move = random.choice(moves)
+        else:
+            move = mc.pick_move()
+            if not move:
+                break
+        new_state = bb.transition(states, move)
+        states.append(new_state)
+        mc.update(new_state)
+        print_board(new_state)
+        i = 1 - i
+    print(bb.winner(mc.states))
+
+if __name__ == '__main__':
+    main()
