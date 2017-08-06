1e14d38e3ed3711f77cc1164961bfc5959957ae9
tombrereton <tbrereton89@gmail.com>
2017-08-06 09:32:04 +0100

    Update: modified script to ignore unfinished files.

--- training_data/sort_data.py
+++ training_data/sort_data.py
@@ -7,8 +7,9 @@ import os
 from ai.state_parser import StateParser
 
 
-def move_to_win_folder(file_name, is_winner):
+def move_to_win_folder(file_name, is_winner, is_finished):
     folder = 'win' if is_winner else 'loss'
+    folder = folder if is_finished else ''
     new_full_file_name = file_name.split('/')
     new_full_file_name.insert(len(new_full_file_name) - 1, folder)
     new_full_file_name = '/'.join(new_full_file_name)
@@ -35,6 +36,7 @@ while files:
         total_medals = None
         medals_uncovered = 0
         line_count = 0
+        game_finished = None
 
         for line in file:
             if line_count == 16:
@@ -43,9 +45,13 @@ while files:
             if line_count > 24 and line_count % 2 == 0:
                 medals_uncovered = line.split('\t')[1]
 
+            if line_count > 24 and line_count % 2 == 1:
+                game_finished = True if line == '-1--1--1--1\n' else False
+
             if medals_uncovered == total_medals:
                 is_file_winner = True
 
             line_count += 1
 
-    move_to_win_folder(full_file_name, is_file_winner)
+    move_to_win_folder(full_file_name, is_file_winner, game_finished)
+    print(game_finished)
