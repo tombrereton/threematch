845e1090ec38695064849f9e19f41578983d5386
elliottdavies <elliottdavies94@gmail.com>
2017-08-25 15:14:50 +0100

    File parser now lists levels as well

--- file_parser/file_parser.py
+++ file_parser/file_parser.py
@@ -1,6 +1,7 @@
 import os
 import random
 import re
+import time
 from itertools import permutations
 
 import numpy as np
@@ -8,8 +9,8 @@ import numpy as np
 
 def shuffle_multiple(*lists):
     """
-        Function to shuffle a list
-        :param lists: A list of lists to shuffle
+        Function to shuffle a list.
+        :param lists: A list of lists to shuffle.
         :return: None
     """
     i = len(lists[0]) - 1
@@ -38,10 +39,10 @@ def one_hot(state, permutation=range(6)):
 
 def reformat_action(action):
     if action[1] == action[3]:
-        # Swap along y axis
+        # Swap along y axis.
         return 0, min(action[0], action[2]), action[3]
     else:
-        # Swap along x axis
+        # Swap along x axis.
         return 1, action[0], min(action[1], action[3])
 
 
@@ -167,16 +168,16 @@ def data_from_generator(generator, steps):
 def splitter(split_fractions, states, actions, labels, game_ids, moves_remaining):
     data_length = len(game_ids)
 
-    # Size of sections
+    # Size of sections.
     foo = [int(data_length * fraction) for fraction in split_fractions]
 
-    # Indices of splits
+    # Indices of splits.
     foo = [data_length - sum(foo[i:]) for i in range(len(split_fractions))]
 
-    # Game IDs at these splits
+    # Game IDs at these splits.
     bar = [game_ids[i] for i in foo]
 
-    # Start of these games
+    # Start of these games.
     baz = [min(j for j in range(max(0, foo[i] - 30), foo[i] + 1) if game_ids[j] == game_id) for i, game_id in
            enumerate(bar)]
 
@@ -189,26 +190,27 @@ def splitter(split_fractions, states, actions, labels, game_ids, moves_remaining
 
 class FileParser:
     """
-        Class to process files in a directory
+        Class to process files in a directory.
     """
 
     def __init__(self):
         """
-            Constructor for class
+            Constructor for class.
         """
-        # Initialise lists for data
+        # Initialise lists for data.
         self.states = []
         self.actions = []
         self.labels = []
         self.game_ids = []
         self.moves_left = []
+        self.levels = []
 
-        # Compile regex patterns for finding lines
+        # Compile regex patterns for finding lines.
         self.info_line_check = re.compile(r'(\d+(\t|(\r?\n))){4}')
         self.state_line_check = re.compile(r'(-?\d+\t)+\r?\n')
         self.action_line_check = re.compile(r'\d+\t[-\d]+\r?\n')
 
-        # Compile regex patterns for breaking lines apart
+        # Compile regex patterns for breaking lines apart.
         self.info_line_parse = re.compile('\d+')
         self.state_line_parse = re.compile('-?\d+')
         self.action_line_parse = re.compile(r'(\d+)\t(-?\d)-(-?\d)-(-?\d)-(-?\d)')
@@ -221,18 +223,18 @@ class FileParser:
                      and the split contents of that line.
         """
         if re.fullmatch(self.info_line_check, line):
-            # Info line
+            # Info line.
             return 0, [int(element) for element in re.findall(self.info_line_parse, line)]
         elif re.fullmatch(self.state_line_check, line):
-            # State line
+            # State line.
             s = np.array([int(element) for element in re.findall(self.state_line_parse, line)], dtype='b')[3:]
             return 1, np.array([int(element) for element in re.findall(self.state_line_parse, line)], dtype='b')
         elif re.fullmatch(self.action_line_check, line):
-            # Action line
+            # Action line.
             m = re.search(self.action_line_parse, line)
             return 2, np.array([int(m.group(i)) for i in range(1, 6)], dtype='b')
         else:
-            # Nothing
+            # Nothing.
             return -1,
 
     def process_file(self, file):
@@ -244,39 +246,41 @@ class FileParser:
         with open(file) as file:
             lines = file.readlines()
 
-        # Initialise variables
+        # Initialise variables.
         medals = None
+        level = None
         moves = None
         states_actions = []
 
-        # Go through lines in the file
+        # Go through lines in the file.
         for line in lines:
-            # Process each line
+            # Process each line.
             result = self.process_line(line)
 
             if result[0] == 0:
-                # This was the info line, get the moves and medals
+                # This was the info line, get the moves and medals.
                 moves = result[1][0]
+                level = {20: 1, 25: 2, 30: 3}[moves]
                 medals = result[1][1]
             elif result[0] != -1:
-                # This was a state or action line
+                # This was a state or action line.
                 states_actions.append(result[1])
 
-        # Sort the lines by line number
+        # Sort the lines by line number.
         states_actions.sort(key=lambda element: element[0])
 
-        # Return medals, moves and the list of states and actions
-        return medals, moves, states_actions
+        # Return medals, moves and the list of states and actions.
+        return level, medals, moves, states_actions
 
     @staticmethod
     def fix(medals, moves, states_actions):
         """
             Method the check of this data usable, and if it needs correcting.
-            :param medals: Number of medals in this level
-            :param moves: Number of moves in this level
-            :param states_actions: List of states and actions
+            :param medals: Number of medals in this level.
+            :param moves: Number of moves in this level.
+            :param states_actions: List of states and actions.
             :return: The fixed data from a file, may return an empty list if
-                      file was too badly corrupted
+                      file was too badly corrupted.
         """
         if 3 < len(states_actions) and medals is not None and moves is not None and \
                         states_actions[-1][1] == -1 and states_actions[-2][0] + 1 == states_actions[-1][0]:
@@ -285,9 +289,9 @@ class FileParser:
             actions = []
 
             for i in range(len(states_actions) - 2):
-                # Check this line is a state
+                # Check this line is a state.
                 if states_actions[i][0] % 2 == 0:
-                    # Check no missing lines
+                    # Check no missing lines.
                     if states_actions[i][0] + 1 == states_actions[i + 1][0]:
                         states.append(states_actions[i])
                         actions.append(states_actions[i + 1])
@@ -296,80 +300,74 @@ class FileParser:
         else:
             return [],
 
-    def add_to_lists(self, game_id, medals, moves, states_actions):
+    def add_to_lists(self, game_id, level, medals, moves, states_actions):
         """
             Method to add all the data into the lists.
             :param game_id: ID of this game.
-            :param medals: Number of medals in this level
-            :param moves: Number of moves in this level
-            :param states_actions: List of states and actions
+            :param level: Level of this game.
+            :param medals: Number of medals in this level.
+            :param moves: Number of moves in this level.
+            :param states_actions: List of states and actions.
             :return: None
             
         """
-        # Fix/check the data
+        # Fix/check the data.
         states, *others = self.fix(medals, moves, states_actions)
 
-        # Unpack others if states is non-empty
+        # Unpack others if states is non-empty.
         if states:
             actions, medals, moves, win = others
 
-        # Add all state/action paris to instance lists along with labels, game_ids and moves_left
+        # Add all state/action paris to instance lists along with labels, game_ids and moves_left.
         for i in range(len(states)):
             self.states.append(states[i][3:])
             self.actions.append(actions[i][1:])
             self.labels.append(win)
             self.game_ids.append(game_id)
             self.moves_left.append(moves - states[i][0] // 2)
+            self.levels.append(level)
 
     def open_files(self, directory=None):
         """
-            Method to read in all files
-            :param directory: Optional argument of the directory to process
+            Method to read in all files.
+            :param directory: Optional argument of the directory to process.
             :return: None
         """
-        # Change directory if required
+        # Change directory if required.
         if directory:
             os.chdir(directory)
 
-        # Get a list of all .txt files
+        # Get a list of all .txt files.
         files = [file for file in os.listdir() if file.find('.txt') != -1]
 
-        # TODO For testing, remove this eventually
+        # TODO For testing, remove this eventually.
         random.shuffle(files)
 
-        # Go through all files, getting the data from them
+        # Go through all files, getting the data from them.
         for game_id, file in enumerate(files):
-            # Get raw data from file
-            medals, moves, states_actions = self.process_file(file)
+            # Get raw data from file.
+            level, medals, moves, states_actions = self.process_file(file)
 
-            # Pass this data for more processing and adding to instance lists
-            self.add_to_lists(game_id, medals, moves, states_actions)
+            # Pass this data for more processing and adding to instance lists.
+            self.add_to_lists(game_id, level, medals, moves, states_actions)
 
         states = np.array(self.states)
         actions = np.array(self.actions)
         labels = np.array(self.labels)
         game_ids = renumber(self.game_ids)
         moves_left = np.array(self.moves_left, dtype='b')
+        levels = np.array(self.levels, dtype='b')
 
         np.save('states', states)
         np.save('actions', actions)
         np.save('labels', labels)
         np.save('game_ids', game_ids)
         np.save('moves_left', moves_left)
+        np.save('levels', levels)
 
 
 if __name__ == '__main__':
-    # t0 = time.time()
-    # FileParser().open_files('data')
-    # t1 = time.time()
-    # print(t1 - t0)
-
-    states = np.transpose(np.reshape(np.load('../ai/data/states.npy'), (-1, 9, 9, 4)), (0, 3, 1, 2))
-    actions = np.load('../ai/data/actions.npy')
-    labels = np.load('../ai/data/labels.npy')
-    game_ids = np.load('../ai/data/game_ids.npy')
-    moves_left = np.load('../ai/data/moves_left.npy')
-
-    for output in data_generator(states, actions, labels, game_ids, moves_left):
-        print(output)
-        break
+    t0 = time.time()
+    FileParser().open_files('data')
+    t1 = time.time()
+    print(t1 - t0)
