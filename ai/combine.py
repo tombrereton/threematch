dcec4c80df938480c11b97be733d960e010d2a1e
elliottdavies <elliottdavies94@gmail.com>
2017-08-29 22:52:38 +0100

    Fixed warnings

--- ai/combine.py
+++ ai/combine.py
@@ -2,12 +2,16 @@ import os
 import re
 import numpy as np
 
-prefix_pattern = re.compile(r'^(.*)\[')
-number_pattern = re.compile(r'\[(\d*)\-')
 
-files = [file for file in os.listdir() if file[-4:] == '.npy']
+def key(element):
+    return re.search(prefix_pattern, element).group(1), int(re.search(number_pattern, element).group(1))
+
+prefix_pattern = re.compile(r'^(.*)\[')
+number_pattern = re.compile(r'\[(\d*)-')
+
+files = [file for file in os.listdir('.') if file[-4:] == '.npy']
 prefixes = sorted({re.search(prefix_pattern, file).group(1) for file in files})
-files.sort(key=lambda element: (re.search(prefix_pattern, element).group(1), int(re.search(number_pattern, element).group(1))))
+files.sort(key=key)
 files = np.reshape(np.array(files), (len(prefixes), -1))
 
 for prefix, sub_files in zip(prefixes, files):
